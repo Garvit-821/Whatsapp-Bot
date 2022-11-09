b2309a4113ac67330e2c70cadf91b93a4f8c64f4
Garvit Prakash @MakeItReal <77687404+Garvit-821@users.noreply.github.com>
2022-11-09 22:13:39 +0530

    Add files via upload

---
+++ G-messenger.py
@@ -0,0 +1,25 @@
+
+import pywhatkit as p
+print("Welcome to G-Messenger")
+print("Developed by Garvit")
+print("Now you can schedule any message!!")
+print("Just fill details whom do you want to send message.")
+print("@MakeItReal")
+print("Follow @factplanet_821 on instagram")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+p.sendwhatmsg(
+
+#input function
+input("Enter mobile no Whom do you want to send message: "),
+input("Enter your Msg: "),
+int(input("Enter time in 24 hour format(hrs): ")),
+int(input("Enter time in 24 hour format(minutes): "))
+
+#this will int will change str to int)
+           )
